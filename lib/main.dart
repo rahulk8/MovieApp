eaab3edd1eb6b735d5f0fa77514d5cb48770970d
rahulk8 <rahulpatna89@gmail.com>
2019-05-26 23:39:56 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'src/app.dart';
+
+void main(){
+  runApp(App());
+}
+
+
+//Source code this project here
+//https://medium.com/flutterpub/architecting-your-flutter-project-bd04e144a8f1
